dc71291e0f4b3863492169ff6dde3a329d51cceb
Ezra Chisale <ezchisale@gmail.com>
2022-10-22 19:02:18 +0000

    task 3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_numbers - print numbers from 0-9
+ *
+ *Return: void
+ */
+
+void print_numbers(void)
+{
+	int i = 0;
+
+	for (i < 10, i++)
+	{
+		_putchar(i);
+		_putchar('\n')
+	}
+}
