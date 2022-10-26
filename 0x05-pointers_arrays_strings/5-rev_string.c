afffeb10a35bd77bfd0910a9210344a58f96379d
Ezra Chisale <ezchisale@gmail.com>
2022-10-26 18:52:00 +0000

    task 6

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ *rev_string -  a function that reverses a string.
+ *@s: string that needs to be reversed
+ *
+ *Return: void
+ */
+void rev_string(char *s);
+{
+	int i = 0, j = 0;
+	char str[5000];
+
+	while (*(s + i))
+	{
+		*(s + i) = *(str + j);
+		j++;
+		i--;
+	}
+}
