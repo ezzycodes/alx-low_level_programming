9b10f924d6f097271f70d5a5efac058657bdcf2a
Ezra Chisale <ezchisale@gmail.com>
2022-10-26 17:09:32 +0000

    task 0

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * swap_int - a function that swaps values of intergers"
+ *
+ *@a: first interger to be swapped
+ *@b: 2nd interger to be swapped
+ *
+ *Return: void
+ */
+
+void swap_int(int *a, int *b);
+{
+	int c;
+
+	c = *a;
+	*a = *b;
+	*b = c;
+}
