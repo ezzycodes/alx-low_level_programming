ab570a1b445c046b65c915359b4f3f140581ff6d
Ezra Chisale <ezchisale@gmail.com>
2022-10-26 20:20:12 +0000

    task 6

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -13,13 +13,13 @@ void puts2(char *str)
 	int a;
 	int b;
 
-	for (a = 0 ; str[a] != '\0' ; i++)
+	for (a = 0 ; str[a] != '\0' ; a++)
 	{
 		;
 	}
 	for (b = 0 ; b < a ; b = b + 2)
 	{
-		_putchar(str[j]);
+		_putchar(str[b]);
 	}
 	_putchar('\n');
 }
