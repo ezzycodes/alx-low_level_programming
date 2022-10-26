f89791daf789a19be3d9afcd41a74bb8bf880096
Ezra Chisale <ezchisale@gmail.com>
2022-10-26 21:06:07 +0000

    task zero

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,5 +1,3 @@
-#ifndef MAIN_H
-#define MAIN_H
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
@@ -11,4 +9,3 @@ char *leet(char *str);
 char *rot13(char *str);
 void print_number(int n);
 int _putchar(char c);
-#endif
