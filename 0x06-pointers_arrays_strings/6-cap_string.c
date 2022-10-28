961daeb9e95d119e97bc65223ea8ac658e19b333
Ezra Chisale <ezchisale@gmail.com>
2022-10-28 16:53:02 +0000

    task 6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -14,6 +14,7 @@ char *cap_string(char *str)
 	{
 		while (!(str[index] >= 'a' && str[index] <= 'z'))
 			index++;
+
 		if (str[index - 1] == ' ' ||
 				str[index - 1] == '\t' ||
 				str[index - 1] == '\n' ||
@@ -27,7 +28,7 @@ char *cap_string(char *str)
 				str[index - 1] == ')' ||
 				str[index - 1] == '{' ||
 				str[index - 1] == '}')
-			index] -= 32;
+			str[index] -= 32;
 	}
 	return (str);
 }
